bbdbc8e778a985f90612d4b078fce7a225747117
GyokayAli <gyokay.metin.ali@gmail.com>
2020-02-19 15:22:20 +0100

    Add service layer

---
+++ Forum.Data/IForum.cs
@@ -0,0 +1,20 @@
+﻿using Forum.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forum.Data
+{
+    public interface IForum
+    {
+        ForumEntity GetById(int id);
+        IEnumerable<ForumEntity> GetAll();
+        IEnumerable<ApplicationUser> GetAllActiveUsers();
+
+        Task Create(ForumEntity forum);
+        Task Delete(int forumId);
+        Task UpdateForumTitle(int forumId, string newTitle);
+        Task UpdateForumDescription(int forumId, string newDescription);
+    }
+}
---
+++ Forum.Service/ForumService.cs
@@ -0,0 +1,55 @@
+﻿using Forum.Data;
+using Forum.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Forum.Service
+{
+    public class ForumService : IForum
+    {
+        private readonly ApplicationDbContext _dbContex;
+
+        public ForumService(ApplicationDbContext dbContext)
+        {
+            _dbContex = dbContext;
+        }
+
+        public Task Create(ForumEntity forum)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int forumId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ForumEntity> GetAll()
+        {
+            return _dbContex.Forums
+                .Include(forum => forum.Posts);
+        }
+
+        public IEnumerable<ApplicationUser> GetAllActiveUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ForumEntity GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateForumDescription(int forumId, string newDescription)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateForumTitle(int forumId, string newTitle)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
